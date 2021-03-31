f89e448a35c9a38f01893c88a15d762c1b37f24a
vavellar <victorcandeias@wine.com.br>
2021-03-31 15:28:35 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+new Vue({
+    el: '#app',
+    data: {
+        round: 0,
+        running: false,
+        playerLife: 100,
+        monsterLife: 100,
+        logs: []
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        },
+
+        youWin() {
+            if (this.monsterLife <= 0) {
+                return 'Ganhou'
+            }
+        },
+
+        youLose() {
+            if (this.playerLife <= 0) {
+                return 'Perdeu'
+            }
+        },
+
+    },
+    methods: {
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.logs = []
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
+        },
+
+        hurt(prop, min, max, especial, source, target, cls) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt} de dano`, cls)
+        },
+
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador se curou em ${heal}.`, 'player')
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
+        }
+
+    },
+    watch: {
+        hasResult(value) {
+            if (value == true) this.running = false
+        }
+    }
+
+})
